0e5be6b3c08e73a878bf933d6feea544ff5b2bdf
Malfazakki <malfazakki@gmail.com>
2023-04-01 11:16:28 +0700

    PHP Modular

--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 <?php 
 include ("koneksi.php");
+require 'header.php';
 
 // Query untuk menampilkan data
 $sql = 'SELECT * FROM data_barang';
 $result = mysqli_query($conn, $sql);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Barang</title>
+
     <style>
         table, td, th{
             border: 1px solid;
